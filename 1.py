9f8a459d75f62eca5be20381d92467068dd3458b
musfirah-kashan <101093830+musfirah-kashan@users.noreply.github.com>
2026-01-21 10:49:31 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,23 @@
+import random
+choice=random.randint(1,50)
+print(choice)
+n=int(input())
+target=n-1
+count=0
+while target!=choice:
+    if n==choice:
+        count+=1
+        print(f"Correct! you guesses it in {count} attempts")
+        target+=1
+
+    elif n>choice:
+        count+=1
+        print(f"That's above the target!")
+        n=int(input())
+
+    else:
+        count+=1
+        print("That's below the target!")
+        n=int(input())
+
+   
